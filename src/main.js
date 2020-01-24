93c5f18dbec764a31d09359b0d5678a175cdf178
wangcheng <453451180@qq.com>
2020-01-24 18:06:08 +0800

    清除一些不需要的组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
@@ -10,5 +9,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
-Vue.use(ElementUI)
