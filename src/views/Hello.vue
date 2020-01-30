4296ac9446c8f355de630531d528e9757e94be47
wangcheng <453451180@qq.com>
2020-01-30 23:40:52 +0800

    header区域布局调整

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="home-container">
-    <el-header>Header</el-header>
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt=""/>
+        <span>XXX后台管理系统</span>
+      </div>
+      <el-button class="info">登出</el-button>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -20,6 +26,19 @@ export default {
   }
   .el-header {
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+    padding-left: 0px;
+    > div {
+      display: flex;
+      align-items: center;
+      > img {
+        width: 40px;
+        height: 40px;
+      }
+    }
   }
   .el-aside {
     background-color: #333744;
